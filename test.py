216983ed7bc9b6ff1cf05d7f5d2921e4c38603d9
CIJIN JOSEPH JOHN <cijinjosephjohn@CijiN-JOseph-JOhn-Mac-Air.local>
2024-09-30 18:22:04 +0530

    Git Test Version 1

---
+++ test.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+num1 = np.array([5,57,3])
+
+num2 = np.array([6,32,3])
+
+result = num1+num2
+
+print(f"num : {num1} + num : {num2} =",end=" ")
+print(f"result : {result}") 
+print("version 1 ")
